3d35309b23359aebe510dcfc0032cbbe18b06aa7
Christian <christianeduncan3@gmail.com>
2019-04-29 12:02:15 -0400

    @kane7890

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,13 @@
+class Ingredient
 
+attr_reader :ingredient
+
+@@all = []
+
+
+def initialize(ingredient)
+  @ingredient = ingredient
+  @@all << self
+end
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,22 @@
 require_relative '../config/environment.rb'
+require 'date'
+
+kane = User.new("Kane")
+christian = User.new("Christian")
+cake = Recipe.new("Cake")
+chicken=Recipe.new("Chicken")
+steak=Recipe.new("Steak")
+pie=Recipe.new("Pie")
+
+recipecard1=RecipeCard.new(5,kane,cake)
+recipecard2=RecipeCard.new(6,kane,chicken)
+recipecard3=RecipeCard.new(7,kane,steak)
+recipecard4=RecipeCard.new(1,kane,pie)
+cinnamon = Ingredient.new("cinnamon")
+chocolate = Ingredient.new("chocolate")
+flour=Ingredient.new("flour")
+allergykane = Allergy.new(kane,cinnamon)
+
+ingredients_arr = [flour,cinnamon,chocolate]
 
 binding.pry
